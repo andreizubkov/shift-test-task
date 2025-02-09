ec26102db3e6a21d02eeb2f3f72b5279ac57f6b7
Zubkov Andrei <andrei.n.zubkov@gmail.com>
2025-02-09 15:41:40 +0700

    переделал порядок считывания строк

--- src/main/java/com/andreizubkov/shift_test_task/fileFilter.java
+++ src/main/java/com/andreizubkov/shift_test_task/fileFilter.java
@@ -2,6 +2,7 @@ package com.andreizubkov.shift_test_task;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -69,17 +70,26 @@ public class fileFilter {
         double avg = 0;
 
         try {
+            BufferedReader[] reader = new BufferedReader[inputs.length];
             for (int i = 0; i < inputs.length; i++) {
-                try (BufferedReader reader = new BufferedReader(new FileReader(inputs[i]))) {
-                    String s;
-                    while ((s = reader.readLine()) != null) {
+                try {
+                    reader[i] = new BufferedReader(new FileReader(inputs[i]));
+                } catch (FileNotFoundException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+            String s;
+            int i = 0;
+            while (true) {
+                try {
+                    if (reader[i] != null && (s = reader[i].readLine()) != null) {
                         if (s.equals("")) continue;
                         try {
                             if (s.contains(".")) {
                                 cur_flt = Double.parseDouble(s);
                                 if (writeFlt == null) writeFlt = new FileWriter(floats, append);
                                 writeFlt.write(s + '\n');
-
+    
                                 flt_num++;
                                 flt_sum = flt_sum.add(new BigDecimal(s));
                                 if (flt_num == 1) {
@@ -93,7 +103,7 @@ public class fileFilter {
                                 cur_int = Long.parseLong(s);
                                 if (writeInt == null) writeInt = new FileWriter(integers, append);
                                 writeInt.write(s + '\n');
-
+    
                                 int_num++;
                                 int_sum += cur_int;
                                 if (int_num == 1) {
@@ -107,7 +117,7 @@ public class fileFilter {
                         } catch (NumberFormatException ex) {
                             if (writeStr == null) writeStr = new FileWriter(strings, append);
                             writeStr.write(s + '\n');
-
+    
                             str_num++;
                             if (str_num == 1) {
                                 str_min = s.length();
@@ -117,9 +127,23 @@ public class fileFilter {
                                 if (s.length() > str_max) str_max = s.length();
                             }
                         }
+                    } else {
+                        if (reader[i] != null) {
+                            reader[i].close();
+                            reader[i] = null;
+                        }
                     }
-                }
-                catch (IOException ex) {
+                    if (i == reader.length - 1) {
+                        i = 0;
+                    } else {
+                        i++;
+                    }
+                    boolean close = true;
+                    for (int j = 0; j < reader.length; j++) {
+                        if (reader[j] != null) close = false;
+                    }
+                    if (close == true) break;
+                } catch (IOException ex) {
                     System.out.println(ex.getMessage());
                 }
             }
